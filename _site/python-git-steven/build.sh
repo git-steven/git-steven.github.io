d187c253492da6b5c5c92da51f2fa270082dff26
Steven Miers <steven.miers@gmail.com>
2026-03-04 15:30:50 -0600

    Whack _site and ignore.

--- _site/python-git-steven/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker image rm -f terracoil/spark
-docker --log-level debug build --progress=plain --no-cache --platform linux/amd64 -t terracoil/spark  .
--- _site/python-git-steven/docker-sh.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run --platform linux/amd64 -v ./data:/data -it --entrypoint sh terracoil/spark
